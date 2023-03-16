b4efc2aedd815004d8f2839092b9c547d0c17869
KisingiMichael <kisingimichael12@gmail.com>
2023-03-16 11:58:03 -0700

    Write a C program which prints a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+*main function prints the phrase "Programming is like building multilingual puzzle"
+*Return 0 if exited properly
+*/
+
+int main(void){
+
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
+}
