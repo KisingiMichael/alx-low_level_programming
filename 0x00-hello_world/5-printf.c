440b4f1aa4a4f9f5d9c0cd39655f8b9532b9727f
KisingiMichael <kisingimichael12@gmail.com>
2023-03-16 13:55:53 -0700

    Printf with betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main -prints the message
- *Return - 0 if exited properly
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void){
-
-
-	printf("with proper grammer, but the outcome is a piece of art, \n");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
