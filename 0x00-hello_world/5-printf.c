a638e924c66849e272a752a76a6cc23993715f7d
KisingiMichael <kisingimichael12@gmail.com>
2023-03-16 13:36:05 -0700

    Print on printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main -prints the message
+ *Return - 0 if exited properly
+ */
+int main(void){
+
+
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
