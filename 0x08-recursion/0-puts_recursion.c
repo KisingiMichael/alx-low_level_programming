6401dd97cedd56ff261bc414c0682a1fc1c111d1
KisingiMichael <kisingimichael12@gmail.com>
2023-04-05 13:31:18 -0700

    print a string followed by new line with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print string
+ * @s: a string to be printed
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+0x08. C - Recursion
+This project is about recursion in C Progamming Language
