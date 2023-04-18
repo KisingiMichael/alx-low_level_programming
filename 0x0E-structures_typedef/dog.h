53ee82e78e968e31174f8a05ba2af0373f1add36
KisingiMichael <kisingimichael12@gmail.com>
2023-04-18 01:26:12 -0700

    modify dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,17 +7,14 @@
  * @age: age
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/**
- * dog_t - typedef for gog structure
- */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif 
