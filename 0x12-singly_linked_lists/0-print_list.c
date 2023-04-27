5f86a2b4ca1b384fd3dfc3cf72d5934492df2d0d
KisingiMichael <kisingimichael12@gmail.com>
2023-04-27 14:47:29 -0700

    Write a function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function to list
+ * @h: List
+ * Return: nodes of the function
+ */
+size_t print_list(const list_t *h)
+{
+	size_t my_nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->node_len, h->str);
+		my_nodes++;
+		h = h->next;
+	}
+	return (my_nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0x12. C - Singly linked lists
+
+This project is all about Singly Linked lists data structures
