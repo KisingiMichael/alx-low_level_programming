e5f24829c0cd81336c41b540d2b55fab22d71daf
KisingiMichael <kisingimichael12@gmail.com>
2023-12-14 00:59:33 -0800

    write a function that searches for a value in a sorted array of integers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -38,4 +38,5 @@ typedef struct skiplist_s
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
